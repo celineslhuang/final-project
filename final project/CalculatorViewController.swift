20da9fded131d37e5dcc3dc188d4157b2b12f5ac
celineslhuang <87721208+celineslhuang@users.noreply.github.com>
2021-07-21 13:41:37 -0700

    coded subtotal calculator view controller, should be fully functional and formatted

---
+++ final project/CalculatorViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CalculatorViewController.swift
+//  final project
+//
+//  Created by Celine Huang on 2021-07-21.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBOutlet weak var originalItemPrice: UITextField!
+    
+    @IBOutlet weak var discountTextField: UITextField!
+
+
+    @IBOutlet weak var taxRateTextField: UITextField!
+    @IBOutlet weak var subtotalLabel: UILabel!
+    
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        let discountAmount = (Double(discountTextField.text!)! / 100)
+        let taxAmount = (Double(taxRateTextField.text!)! / 100)
+        let originalPrice = Double(originalItemPrice.text!)!
+        let subtotal = (originalPrice - (discountAmount * originalPrice)) * (taxAmount + 1)
+        let subtotalRounded = Double(round(100*subtotal)/100)
+        subtotalLabel.text = "$ \(subtotalRounded)"
+    }
+  
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
